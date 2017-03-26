871b73c353d820d5feee67fa7daf66200f67aed3
darreny12 <darreny12@yahoo.com>
2017-03-26 11:02:53 -0700

    Add files via upload

--- app/src/main/java/com/example/hshack/circadian/DashboardFragment.java
+++ app/src/main/java/com/example/hshack/circadian/DashboardFragment.java
@@ -2,24 +2,67 @@ package com.example.hshack.circadian;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.ibm.watson.developer_cloud.conversation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.hshack.circadian.R;
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
+    private ArrayList<Message> messageArrayList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2016_07_11);
-        service.setUsernameAndPassword("<username>", "<password>");
+        final View v = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        Button button = (Button) v.findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText tv = (EditText) view.findViewById(R.id.editText2);
+                final ConversationService service = new ConversationService("2017-03-26");
 
-        MessageRequest newMessage = new MessageRequest.Builder().inputText("Hi").build();
-        MessageResponse response = service.message("<workspace-id>", newMessage).execute();
-        System.out.println(response);
+                service.setUsernameAndPassword("3a0d0775-006f-45af-a971-eac23a4577fe", "DCpBzpq76lPV");
 
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+                String text = " ";
+                if(tv != null)
+                    text = tv.getText().toString();
+                final MessageRequest newMessage = new MessageRequest.Builder()
+                        .inputText(text)
+                        .build();
+
+                GridLayout l = (GridLayout) v.findViewById(R.id.grid);
+                TextView temp = new TextView(getContext());
+                temp.setText(text);
+                l.addView(temp);
+
+
+                Thread thread = new Thread(new Runnable() {
+                    public void run() {
+                        try {
+                            MessageResponse response = service.message("25dfa8a0-0263-471b-8980-317e68c30488", newMessage).execute();
+                            System.out.println("test");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+        });
+        return v;
     }
+
+
+
 }
